43a20edcf53d17c5132127a9ef8d457eb73f8ede
Trevor J. Schmitz <schmitzt3@gmail.com>
2016-02-26 11:10:51 -0500

    Added game html.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,9 +1,22 @@
 (function () {
 	angular
-		.module('app', ['ui-router'])
-		.config(appConfig);
+		.module('app', ['ui.router'])
+		.config(appConfig, ['$locationProvider', '$stateProvider', '$urlRouterProvider']);
 	
-	function appConfig() {
-		console.log('config');
+	function appConfig($locationProvider, $stateProvider, $urlRouterProvider) {
+		console.log($stateProvider);
+		$locationProvider.html5Mode(true);
+		$urlRouterProvider.otherwise('/');
+		 
+		$stateProvider
+			.state('app', {
+      			url: '',
+				template: '<div ui-view=""></div>',
+				abstract: true
+    		})
+			.state('app.game', {
+				url: '/game',
+				templateUrl: '/app/partials/game.html'
+			});
 	}
 })();
