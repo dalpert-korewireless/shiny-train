1f8bd178ab0e94436148d52dd2a27fd8470ef47c
Trevor J. Schmitz <schmitzt3@gmail.com>
2016-02-26 13:33:05 -0500

    Fixed issue with logging.

--- ShinyTrain/ShinyTrain/Hubs/ShinyHub.cs
+++ ShinyTrain/ShinyTrain/Hubs/ShinyHub.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Common.Logging;
+﻿using Common.Logging;
 using Microsoft.AspNet.SignalR;
 
 namespace ShinyTrain.Hubs
